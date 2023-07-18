b9eceb47d700fbcabe9d603f44b0da95fc9ad67b
syazell1 <mafty.erin01@gmail.com>
2023-07-18 20:25:16 +0800

    update global components

--- client/src/components/layout/Container.tsx
+++ client/src/components/layout/Container.tsx
@@ -7,7 +7,7 @@ type ContainerType = {
 }
 
 const Container  = (props : ContainerType) => {
-    const c = twMerge("max-w-2xl mx-auto min-h-screen px-2", props.className)
+    const c = twMerge("max-w-2xl mx-auto min-h-screen px-2 py-11", props.className)
 
     return (
         <main className={c}>
