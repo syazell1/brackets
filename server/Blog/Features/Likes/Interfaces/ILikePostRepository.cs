46acdfc186caf85fc7a9a365cc0ccc19f583fd17
syazell1 <mafty.erin01@gmail.com>
2023-07-20 20:48:42 +0800

    add likes and comments count

--- server/Blog/Features/Likes/Interfaces/ILikePostRepository.cs
+++ server/Blog/Features/Likes/Interfaces/ILikePostRepository.cs
@@ -4,5 +4,5 @@ using Blog.Entities;
 namespace Blog.Features.Likes.Interfaces;
 public interface ILikePostRepository : IWriteRepository<LikePost>, IReadRepository<LikePost>
 {
-    
+
 }
--- server/Blog/Features/Posts/Dtos/PostsDto.cs
+++ server/Blog/Features/Posts/Dtos/PostsDto.cs
@@ -2,4 +2,11 @@ using Blog.Features.Users.Dtos;
 
 namespace Blog.Features.Posts.Dtos;
 
-public sealed record PostsDto(Guid Id, string Title, string Content, UserDto Owner, DateTime CreatedAt);
+public sealed record PostsDto(
+    Guid Id, 
+    string Title, 
+    string Content, 
+    int LikeCount,
+    int CommentCount,
+    UserDto Owner, 
+    DateTime CreatedAt);
