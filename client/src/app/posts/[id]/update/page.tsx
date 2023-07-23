e841a63bea7d1f77b19642e5d1b90cbf65282d03
syazell1 <mafty.erin01@gmail.com>
2023-07-23 09:56:03 +0800

    update protected routing

--- client/src/app/posts/[id]/update/page.tsx
+++ client/src/app/posts/[id]/update/page.tsx
@@ -1,11 +1,16 @@
 import Container from "@/components/layout/Container";
 import UpdatePostForm from "@/features/Posts/components/UpdatePostForm";
+import { getUser } from "@/providers/getCurrentUser";
+import { redirect } from "next/navigation";
 
 type PostDetailsPageParamsType = {
     id: string
 }
-const UpdatePostPage = ({params}: {params: PostDetailsPageParamsType}) => {
-    console.log(params.id);
+const UpdatePostPage = async ({params}: {params: PostDetailsPageParamsType}) => {
+        const user = await getUser();
+
+        if (user == null )
+            redirect('/')
 
     return (
         <Container className="pt-[20px]">
