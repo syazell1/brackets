8ec3f4897c221400373261e4d34b1ec00a9ee4a4
syazell1 <mafty.erin01@gmail.com>
2024-01-20 14:23:50 +0800

    add create post page

---
+++ client/app/posts/create/page.tsx
@@ -0,0 +1,11 @@
+import AddPostForm from "client/features/posts/components/AddPostForm"
+
+const CreatePostPage = () => {
+  return (
+    <>
+      <AddPostForm />
+    </>
+  )
+}
+
+export default CreatePostPage
