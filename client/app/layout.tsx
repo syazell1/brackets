e295574435aa37672f7f946956f369b3d8b6c3f9
syazell1 <mafty.erin01@gmail.com>
2024-02-04 21:00:58 +0800

    update button

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div id='modal-root'></div>
         <Toaster />
         <QueryProvider>
           <AuthContext>
