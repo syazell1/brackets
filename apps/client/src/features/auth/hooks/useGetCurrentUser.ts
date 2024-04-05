3d92d20e13976c2b4706e7cfecb7db4995a700da
syazell1 <mafty.erin01@gmail.com>
2024-04-06 06:36:58 +0800

    update hook return type w/ isLoggedIn flag

--- apps/client/src/features/auth/hooks/useGetCurrentUser.ts
+++ apps/client/src/features/auth/hooks/useGetCurrentUser.ts
@@ -1,12 +1,14 @@
 import { useAxios } from "@/hooks/useAxios"
 import { useQuery } from "@tanstack/react-query"
 import { AuthInfo } from "../types/auth.types";
+import { useContext } from "react";
+import { authContextProvider } from "@/providers/AuthContext";
 
 export const useGetCurrentUser = () => {
     const client = useAxios();
+    const {isLoggedIn} = useContext(authContextProvider)
 
-
-    return useQuery({
+    const queryResult = useQuery({
         queryFn: async () => {
             const res = await client.get<AuthInfo>('/auth/current_user');
 
@@ -14,6 +16,12 @@ export const useGetCurrentUser = () => {
         },
         queryKey: ["current_user"],
         retry: 1,
-        refetchOnWindowFocus: false
+        refetchOnWindowFocus: false,
+        refetchOnMount: false
     })
+
+    return {
+        isLoggedIn,
+        ...queryResult
+    }
 }
