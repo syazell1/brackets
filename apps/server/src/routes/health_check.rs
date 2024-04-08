7bbe3fa77bea68d7a5d53d5320bd4425a76aa74a
syazell1 <mafty.erin01@gmail.com>
2024-04-08 08:27:36 +0800

    remove turbo repo

--- apps/server/src/routes/health_check.rs
+++
@@ -1,8 +0,0 @@
-use actix_web::HttpResponse;
-
-#[tracing::instrument(
-    name = "Checking Endpoint"
-)]
-pub async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
--- packages/ui/src/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-type ErrorMessageType = {
-  message: String
-}
-
-const ErrorMessage = ({ message }: ErrorMessageType) => {
-  return <p className="text-red-500 text-sm font-semibold">{message}</p>
-}
-
-export default ErrorMessage;
