6903c36a371b788f7ef3e17542c17c021e996755
syazell1 <mafty.erin01@gmail.com>
2024-04-20 12:55:55 +0800

    add user info and date

--- client/src/features/comments/components/CommentsItem.tsx
+++ client/src/features/comments/components/CommentsItem.tsx
@@ -4,11 +4,12 @@ import { CommentsData } from "../types/comments.type"
 import { forwardRef, useContext, useState } from "react"
 import UpdateCommentForm from "./UpdateCommentForm"
 import { authContextProvider } from "@/providers/AuthContext"
-import { Card } from "@/components/ui/card"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import remarkGfm from 'remark-gfm'
 import ReactMarkdown from 'react-markdown'
 import CommentItemMenu from "./CommentItemMenu"
 import DeleteCommentDialog from "./DeleteCommentDialog"
+import UserAvatar from "@/features/users/components/UserAvatar"
 
 type CommentsItemType = {
   data: CommentsData
@@ -20,46 +21,59 @@ const CommentsItem = forwardRef<HTMLElement, CommentsItemType>(
     const [isUpdate, setIsUpdate] = useState(false);
     const [isDelete, setIsDelete] = useState(false);
 
+    const createdDate = new Date(data.created_at);
+
     const commentItemContent = (
       <>
-        <DeleteCommentDialog commentId={data.id} postId={data.post_id} isOpen={isDelete} setIsOpen={setIsDelete}/>
-        <Card className="flex p-6">
+        <DeleteCommentDialog commentId={data.id} postId={data.post_id} isOpen={isDelete} setIsOpen={setIsDelete} />
+        <Card>
           {isUpdate ? (
             <>
               <UpdateCommentForm
                 setUpdateCommentHandler={() => setIsUpdate(false)}
                 commentData={data} />
             </>
-        ) : (
-        <>
-          <div className="flex-1">
-            <div className={`markdown-body prose light rounded-md`} >
-              <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {data.content}
-              </ReactMarkdown>
-            </div>
-          </div>
-          {(isLoggedIn && data.owner.username == usersInfo.username) &&
-            <CommentItemMenu
-              setUpdateCommentHandler={() => setIsUpdate(true)}
-              setDeleteCommentHandler={() => setIsDelete(true)}
-            />
-          }
-        </>
+          ) : (
+            <>
+            <CardHeader className="flex flex-row items-center justify-between">
+              <div className="flex gap-2 items-center">
+                <div>
+                  <UserAvatar />
+                </div>
+                <div>
+                  <p className="font-semibold">{data.owner.username}</p>
+                  <p className="text-xs">{createdDate.toDateString()}</p>
+                </div>
+              </div>
+              {(isLoggedIn && data.owner.username == usersInfo.username) &&
+                <CommentItemMenu
+                  setUpdateCommentHandler={() => setIsUpdate(true)}
+                  setDeleteCommentHandler={() => setIsDelete(true)}
+                />
+              }
+            </CardHeader>
+                <CardContent >
+                  <div className={`markdown-body prose light rounded-md`} >
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                      {data.content}
+                    </ReactMarkdown>
+                  </div>
+                </CardContent>
+            </>
           )}
-      </Card>
+        </Card>
       </>
     )
 
-return ref ? (
-  <article ref={ref}>
-    {commentItemContent}
-  </article>
-) : (
-  <article>
-    {commentItemContent}
-  </article>
-)
+    return ref ? (
+      <article ref={ref}>
+        {commentItemContent}
+      </article>
+    ) : (
+      <article>
+        {commentItemContent}
+      </article>
+    )
   }
 )
 
