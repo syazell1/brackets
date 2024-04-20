96d9784d3edfb6f2f3634de7b797613c7e24fb11
syazell1 <mafty.erin01@gmail.com>
2024-04-20 12:13:17 +0800

    add optimistic updates on the hook

--- client/src/features/comments/hooks/useDeleteComment.ts
+++ client/src/features/comments/hooks/useDeleteComment.ts
@@ -1,14 +1,65 @@
-import { useMutation } from "@tanstack/react-query"
-import { deleteComments } from "../services/comments.api.service"
+import { useAxios } from "@/hooks/useAxios"
+import { PageList } from "@/types/page-lists.types";
+import { InfiniteData, useMutation, useQueryClient } from "@tanstack/react-query"
 import toast from "react-hot-toast"
+import { CommentsData } from "../types/comments.type";
+
+type useDeleteCommentType = {
+  commentId : string,
+  postId : string
+}
 
 export const useDeleteComment = () => {
+  const client = useAxios();
+  const queryClient = useQueryClient();
+
   return useMutation({
-    mutationFn: (id: string) => {
-      return deleteComments(id)
+    mutationFn: async (data: useDeleteCommentType) => {
+      const res = await client.delete(`comments/${data.commentId}`);
+
+      return res.data;
+    },
+    onMutate: async ({postId, commentId}: useDeleteCommentType) => {
+      await queryClient.cancelQueries({queryKey : ["comments", {postId}]})
+
+      const prevComments = queryClient.getQueryData<InfiniteData<PageList<CommentsData[]>>>(["comments", {postId}])
+
+      queryClient.setQueryData(['comments', {postId}], (old : InfiniteData<PageList<CommentsData[]>>) => {
+
+
+        const newData = old.pages.map(v => {
+          let commentsData = v.data.filter(v => {
+            if(v.id !== commentId) {
+              return v;
+            }
+          })
+          
+          return {
+            data : commentsData,
+            page_metadata : v.page_metadata
+          }
+        });
+
+        return {
+          pages: newData,
+          pageParams: old.pageParams
+        }
+      })
+
+      return {prevComments}
+    },
+    onError: (err, {postId}, context) => {
+      queryClient.setQueryData(["comments", {postId}], context?.prevComments)
+
+      toast.error(err.message)
     },
     onSuccess: () => {
       toast.success("Comment successfully added!")
+    },
+    onSettled:  (d, e, {postId}) =>{
+      queryClient.invalidateQueries({
+        queryKey: ["comments", {postId}]
+      })
     }
   })
 }
--- client/src/features/comments/hooks/useUpdateComment.ts
+++ client/src/features/comments/hooks/useUpdateComment.ts
@@ -1,7 +1,8 @@
-import { useMutation } from "@tanstack/react-query"
-import { UpdateCommentInput } from "../types/comments.type"
-import { updateComments } from "../services/comments.api.service"
+import { InfiniteData, useMutation, useQueryClient } from "@tanstack/react-query"
+import { CommentsData, UpdateCommentInput } from "../types/comments.type"
 import toast from "react-hot-toast"
+import { useAxios } from "@/hooks/useAxios"
+import { PageList } from "@/types/page-lists.types"
 
 type useUpdateCommentType = {
   id: string,
@@ -9,15 +10,57 @@ type useUpdateCommentType = {
 }
 
 export const useUpdateComment = () => {
+  const client = useAxios();
+  const queryClient = useQueryClient(); 
+
   return useMutation({
-    mutationFn: ({ id, data }: useUpdateCommentType) => {
-      return updateComments(id, data)
+    mutationFn: async ({ id, data }: useUpdateCommentType) => {
+      const res = await client.patch(`comments/${id}`, data);
+
+      return res.data;
+    },
+    onMutate: async ({id, data}) => {
+      await queryClient.cancelQueries({queryKey: ["comments", {postId : data.post_id}]})
+
+
+      const prevComments = queryClient.getQueryData<InfiniteData<PageList<CommentsData[]>>>(["comments", {postId : data.post_id}])
+
+      queryClient.setQueryData(['comments', {postId : data.post_id}], (old : InfiniteData<PageList<CommentsData[]>>) => {
+        const newData = old.pages.map(v => {
+
+          let commentsData = v.data.map(v => {
+            if(v.id === id) {
+              v.content = data.content
+            }
+            return v;
+          })
+
+          return {
+            data : commentsData,
+            page_metadata : v.page_metadata
+          }
+        });
+
+        return {
+          pages: newData,
+          pageParams: old.pageParams
+        }
+      })
+
+      return { prevComments }
     },
     onSuccess: () => {
       toast.success("Comment updated successfully")
     },
-    onError: () => {
-      toast.error("Comment was not updated.")
+    onError: (err, {data}, context) => {
+      queryClient.setQueryData(["comments", {postId : data.post_id}], context?.prevComments)
+
+      toast.error(err.message)
+    },
+    onSettled: (d, e, {data}) => {
+      queryClient.invalidateQueries({
+        queryKey: ["comments",  {postId : data.post_id}]
+      })
     }
   })
 } 
